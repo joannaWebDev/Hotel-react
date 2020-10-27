f7f74529f74a2681694749056e0d531714f801cf
joannaWebDev <joannasmerea@gmail.com>
2020-10-27 19:26:40 +0100

    modified paths

--- src/components/Bookings.js
+++ src/components/Bookings.js
@@ -1,7 +1,5 @@
 import React from "react";
-import Search from "./Search.js";
-// import SearchResults from "./SearchResults.js";
-// import FakeBookings from "./data/fakeBookings.json";
+import Search from "./Search";
 
 const Bookings = () => {
   const search = searchVal => {
