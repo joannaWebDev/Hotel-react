a9d9fbd731aeebf65d586f5e9c17de2e16859689
joannaWebDev <joannasmerea@gmail.com>
2020-10-31 14:32:02 +0100

    Updates

---
+++ precache-manifest.a0d6ed0de446a890de9757858eb53059.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ac5c69b27cbea551fd60f30c1bd54eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1dd982c4bfc903aec39e",
+    "url": "/static/css/main.6e06b90d.chunk.css"
+  },
+  {
+    "revision": "dc37c182e3d7dc31d9e8",
+    "url": "/static/js/2.0c6e994a.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/static/js/2.0c6e994a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dd982c4bfc903aec39e",
+    "url": "/static/js/main.c0bce64b.chunk.js"
+  },
+  {
+    "revision": "4c7ee2dc1d082c23d2f6",
+    "url": "/static/js/runtime-main.42aab16e.js"
+  }
+]);
